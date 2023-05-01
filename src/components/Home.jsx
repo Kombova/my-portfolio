c0bf2d13fbfed7dc00221eb7a6d6d4d58f6e99d5
Bohdane <berlo1113@gmail.com>
2023-05-01 03:47:46 +0300

    new file:   src/assets/me.jpg 	modified:   src/assets/photo.png 	modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { motion, useAnimate, usePresence } from "framer-motion";
 import { useEffect, useCallback} from "react";
 import { swipeControle } from "../logic/swipeControle";
-import photo from '../assets/photo_2.png';
+import photo from '../assets/photo.png';
 import arrow_icon from '../assets/arrow.svg';
 const Home = ({setScreen}) => {
     const [isPresent, safeToRemove] = usePresence();
@@ -32,7 +32,7 @@ const Home = ({setScreen}) => {
 
     return(
         <motion.div 
-            className="home w-full h-full p-[10px] bg-slate-50 bg-opacity-[0.1]  rounded-[10px] flex flex-wrap justify-center"
+            className="home w-full h-full p-[10px] bg-slate-50 bg-opacity-[0.1]  rounded-[10px] gap-10 flex flex-col justify-center"
             initial={swipeControle.swipe === 'Left' ? {translateX: '100vw', top:'10px'} :
                      swipeControle.swipe === 'Right' ? {translateX: '-100vw', top:'10px'} :
                      swipeControle.swipe === 'Up' ? {translateY: '100%', top:'10px'} : {translateY: '-100%' ,top:'10px'} 
@@ -43,15 +43,15 @@ const Home = ({setScreen}) => {
             ref={scope}
         >
             <h6>ABOUT ME</h6>
-            <img src={photo} alt='me'  className='rounded w-[300px] mx-auto' />
-            <p className=" ">
+            <img src={photo} alt='me'  className='rounded w-full max-w-[1300px] max-h-[70vh]  mx-auto ' />
+            <p className=" opacity-80 max-w-[1000px] text-center  mx-auto ">
                 Hello, my name is Bohdan and I am a frontend developer. I have experience in creating web applications. I am looking for a job both in an office and remotely, with the opportunity to work in a team where I can learn new things and contribute to the overall success of projects. I am ready to start working immediately and willing to take on any technical challenges that may arise. Thank you for your attention, I hope for successful cooperation with you. If you have any questions or interest in my work, please feel free to contact me.
             </p>
             
-            <img src={arrow_icon} alt='arrow' className='absolute top-[-18px] rotate-90 y_up_array_animate' />
-            <img src={arrow_icon} alt='arrow' className='absolute  top-[50vh] rotate-180 x_right_array_animate ' />
+            <img src={arrow_icon} alt='arrow' className='absolute top-[-18px] left-1/2 transform -translate-x-1/2 rotate-90 y_up_array_animate' />
+            <img src={arrow_icon} alt='arrow' className='absolute  top-[50vh]  rotate-180 x_right_array_animate ' />
             <img src={arrow_icon} alt='arrow' className='absolute left-[-18px] top-[50vh] x_left_array_animate' />
-            <img src={arrow_icon} alt='arrow' className='absolute bottom-[-18px] rotate-[-90deg] y_down_array_animate' />
+            <img src={arrow_icon} alt='arrow' className='absolute bottom-[-18px] left-1/2 transform -translate-x-1/2 rotate-[-90deg] y_down_array_animate' />
         </motion.div>        
     );
 };
